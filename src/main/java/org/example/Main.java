8f794979df746f8250f15a4b8fc87ab012739392
rajatkinlekar <kinlekarrajat@gmail.com>
2025-05-17 22:11:45 +0530

    updated getprefixmethod and added rangesum method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,19 +7,30 @@ public class Main {
         int[] nums = {4, 6, 2, 8, 2};
         int[] prefix = new Main().getPrefixSumArray(nums);
         System.out.println(Arrays.toString(prefix));
+        System.out.println(new Main().getSumOfRange(nums, 1, 4));
     }
 
     public int[] getPrefixSumArray(int[] nums) {
         int[] prefixSum = new int[nums.length + 1];
         prefixSum[0] = 0;
-        int sum = 0;
+
 
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            prefixSum[i + 1] = sum;
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
 
         return prefixSum;
+    }
+
+    public int getSumOfRange(int[] nums, int left, int right) {
+        if (left < 0 || right > nums.length) {
+            throw new RuntimeException("left or right pointers are incorrect");
+        }
+
+        // get prefix sum array
+        int[] prefix = this.getPrefixSumArray(nums);
+
+        return prefix[right + 1] - prefix[left];
     }
 
 
